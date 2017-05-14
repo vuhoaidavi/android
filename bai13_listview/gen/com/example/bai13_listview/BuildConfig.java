73ec8d8be617782a0775dbdd5640b9ea1e689aca
vuhoaidavi631996@gmail.com <vuhoaidavi631996@gmail.com>
2017-05-14 20:56:08 +0700

    Uk, t  la Vi ne

---
+++ bai13_listview/gen/com/example/bai13_listview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai13_listview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ bai13_listview_xml/gen/com/example/bai13_listview_xml/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai13_listview_xml;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
