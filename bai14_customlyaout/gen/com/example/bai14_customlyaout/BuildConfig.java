cbdbd603e8e948ad11f47dd6c877c3b65777346b
vuhoaidavi631996@gmail.com <vuhoaidavi631996@gmail.com>
2017-05-25 19:59:09 +0700

    bai14

---
+++ bai14_customlyaout/gen/com/example/bai14_customlyaout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai14_customlyaout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ bai14_customlyaout/src/com/example/bai14_customlyaout/Employee.java
@@ -0,0 +1,30 @@
+package com.example.bai14_customlyaout;
+
+
+public class Employee {
+	 private String id;
+	 private String name;
+	 private boolean gender;
+	 public String getId() {
+	 return id;
+	 }
+	 public void setId(String id) {
+	 this.id = id;
+	 }
+	 public String getName() {
+	 return name;
+	 }
+	 public void setName(String name) {
+	 this.name = name;
+	 }
+	 public boolean isGender() {
+	 return gender;
+	 }
+	 public void setGender(boolean gender) {
+	 this.gender = gender;
+	 }
+	 @Override
+	 public String toString() {
+	 return this.id+"-"+this.name;
+	 }
+	}
